81e9bd62b312a0dfa015f1202e847359e2fe0b9f
criops <criops@criops>
2023-05-18 22:08:55 +0300

    trying tot deploy

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      mono: ["JetBrains Mono", "monospace"],
+    },
+    colors: {
+      'regal-blue': '#243c5a',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
